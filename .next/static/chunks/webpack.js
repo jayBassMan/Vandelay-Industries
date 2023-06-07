615c12601195ff4281118d3bb9bdb3db9f66ce32
Jonathan Johnson <jonathan.johnson@hydradynellc.com>
2023-06-07 16:15:56 -0500

    Adding images to project

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d83465bf0d52db74"; }
+/******/ 		__webpack_require__.h = function() { return "628fcf1ee8e6a761"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 import { BsFillMoonStarsFill } from 'react-icons/bs';
+import { AiFillTwitterCircle, AiFillLinkedin, AiFillYoutube } from "react-icons/ai";
 import Image from 'next/image';
 
 
@@ -16,7 +17,7 @@ const Home = () => {
             <main className='bg-white px-10'>
                 <section className="min-h-screen">
                     <nav className='py-10 mb-12 flex justify-between'>
-                    <h1 className='text-xl'>Developed By Jon!</h1>
+                    <h1 className='text-xl font-burtons'>Developed By Jon!</h1>
                     <ul className='flex items-center'>
                         <li>
                             <BsFillMoonStarsFill />
@@ -24,7 +25,19 @@ const Home = () => {
                         <li><a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="#">Resume</a></li>
                     </ul>
                     </nav>
-                    
+                    <div className='text-center p-10'>
+                        <h2>John John</h2>
+                        <h3>Developer and designer.</h3>
+                        <p>Freelancer providing services for programing and needs. Join me down below and lets get cracking!</p>
+                    </div>
+                    <div>
+                        <AiFillTwitterCircle />
+                        <AiFillLinkedin />
+                        <AiFillYoutube />
+                    </div>
+                    <div>
+
+                    </div>
                 </section>
             </main>
         </div>
